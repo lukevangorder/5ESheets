8f507e44ef694a2311975855f9f22e3ebf5b2c0b
lukevangorder <lukevangorder@gmail.com>
2021-05-31 14:18:41 -0400

    Adds CSheet class to test main body of character sheet

--- 5e-sheets/src/App.js
+++ 5e-sheets/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // return ();
+  return (
+    <div class='CSheet'>Character Sheet Body</div>
+  );
 }
 
 export default App;
