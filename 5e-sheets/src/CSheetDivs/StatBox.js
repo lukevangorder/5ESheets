e64f18775bf697605ef72f67b21fb05a6e97dd8d
lukevangorder <lukevangorder@gmail.com>
2021-06-01 17:30:12 -0400

    Adds event listeners to plus and minus buttons to increment and decrease statBox values

--- 5e-sheets/src/CSheetDivs/StatBox.js
+++ 5e-sheets/src/CSheetDivs/StatBox.js
@@ -8,11 +8,30 @@ export class StatBox extends Component {
             value: 10
         }
     }
+
+    handleIncrease = () => {
+        const newStat = this.state.value + 1
+        this.setState({
+            value: newStat
+        })
+    }
+
+    handleDecrease = () => {
+        const newStat = this.state.value - 1
+        this.setState({
+            value: newStat
+        })
+    }
+
     render() {
         return (
             <div class='statBox grow'>
                 <div class='statBoxStat'>{this.state.value}</div>
-                <div class='statBoxLabel'>{this.props.stat}</div>
+                <div class='statBoxLabel'>
+                    {this.props.stat}
+                    <a class='cursorHover' onClick={this.handleDecrease}>  -  </a>
+                    <a class='cursorHover' onClick={this.handleIncrease}>  +  </a>
+                </div>
             </div>
         )
     }
